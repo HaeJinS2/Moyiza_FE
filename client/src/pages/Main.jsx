863bf196302a2d8b5b3cbc9a952b64df29547cbe
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-05-22 21:42:59 +0900

    Design: Design main page
    메인페이지 디자인 중

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import CreateClub from './CreateClub'
+import React from "react";
+import Navbar from "../component/Navbar";
+import CreateClub from "./CreateClub";
 
 function Main() {
   return (
-    <div>
-      <CreateClub />
-    </div>
-  )
+    <>
+      <Navbar />
+      <div className=" h-screen flex items-center justify-center">
+        <div>
+          <div>몸과 마음이</div>
+          <div>건강해지는 습관 만들기</div>
+          <div>대한민국 1등 건강습관 앱, 챌린저스</div>
+        </div>
+        <div>
+          <img src={`${process.env.PUBLIC_URL}/images/cat.jpeg`} />
+        </div>
+      </div>
+      <div className=" h-screen flex items-center justify-center">dd</div>
+      <div className=" h-screen flex items-center justify-center">dd</div>
+      <div>
+        <CreateClub />
+      </div>
+    </>
+  );
 }
 
-export default Main
+export default Main;

520c772bc228b7d37b770767b49ab1edd53f120f
HaeJinS2 <9674qzq@gmail.com>
2023-05-22 18:21:57 +0900

    feat: Create components to club creation

---
+++ client/src/pages/CreateClub.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { useRecoilState } from 'recoil';
+import { clubState, tempIdState } from '../states/clubState';
+import {postAPI} from "../axios";
+import { useNavigate } from "react-router-dom";
+
+
+function CreateClub() {
+    const navigate = useNavigate();
+    const [club, setClub] = useRecoilState(clubState);
+    const [tempId, setTempId] = useRecoilState(tempIdState);
+
+    const handleCreateClubButton = () => {
+        postAPI("/create", {}
+        ).then((response) => {
+            if (response.status === 201) {
+                console.log(response);
+                setTempId(response.data.tempId)
+                setClub({
+                    ...club,
+                    category: response.data.category,
+                    tag: response.data.tag,
+                    title: response.data.title,
+                    content: response.data.content,
+                    restriction: response.data.restriction,
+                });
+            } else if (response.status === 200) {
+                setTempId(response.data.tempId)
+                console.log(response);
+            }
+            navigate(`/create-club-form`)
+        }).catch((error) => {
+            console.error(error)
+        })
+    }
+
+    return (
+        <div>
+            <button onClick={handleCreateClubButton}>클럽 만들기 버튼</button>
+        </div>
+    )
+}
+
+
+export default CreateClub

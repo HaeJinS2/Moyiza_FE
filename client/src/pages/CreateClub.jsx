8c6f8fa46b4b1e619f7467d72bd32e4554651903
HaeJinS2 <9674qzq@gmail.com>
2023-05-22 23:33:05 +0900

    Refactor: Modify handleCreateClubButton

--- client/src/pages/CreateClub.jsx
+++ client/src/pages/CreateClub.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useRecoilState } from 'recoil';
-import { clubState, tempIdState } from '../states/clubState';
+import { clubState, optionState } from '../states/clubState';
 import { getAPI, postAPI } from "../axios";
 import { useNavigate } from "react-router-dom";
 
@@ -8,35 +8,51 @@ import { useNavigate } from "react-router-dom";
 function CreateClub() {
     const navigate = useNavigate();
     const [club, setClub] = useRecoilState(clubState);
-    const [tempId, setTempId] = useRecoilState(tempIdState);
-    console.log(tempId)
+    // const [tempId, setTempId] = useRecoilState(tempIdState);
+    const [option, setOption] = useRecoilState(optionState);
 
     const handleCreateClubButton = () => {
         postAPI("/club/create", {}).then((response) => {
-            setTempId(response.data.tempId);
-    
-            getAPI(`/club/create/${response.data.tempId}`).then((getResponse) => {
-                if (getResponse.status === 201) {
+            if (response.status === 201) { // 임시저장된 데이터가 있는 경우
+                getAPI(`/club/create`).then((getResponse) => {
                     if (window.confirm('불러오시겠습니까?')) {
-                        // Load the saved data
+                        // setTempId(response.data.createclub_id)
                         setClub({
                             ...club,
-                            category: getResponse.data.category,
-                            tag: getResponse.data.tag,
-                            title: getResponse.data.title,
+                            createclub_id: getResponse.data.createclub.createclub_id,
+                            category: getResponse.data.createclub.clubcategory,
+                            tag: getResponse.data.createclub.clubTag,
+                            title: getResponse.data.createclub.clubTitle,
                             content: getResponse.data.content,
-                            restriction: getResponse.data.restriction,
+                            restriction: getResponse.data.restriction, 
                         });
+                        setOption({
+                            ...option,
+                            optionList: getResponse.data.optionList,
+                        })
+
                     }
-                }
-                navigate(`/create-club-form`)
-            }).catch((error) => {
-                console.error(error);
-            });
+                    navigate(`/create-club-form`);
+                }).catch((error) => {
+                    console.error(error);
+                });
+            } else if (response.status === 200) { // 임시저장된 데이터가 없는 경우
+                // setTempId(response.data.createclub_id); // tempId 발급 받음
+                setClub({
+                    ...club,
+                    createclub_id: response.data.createclub_id,
+                })
+                setOption({
+                    ...option,
+                    optionList: response.data.optionList,
+                })
+                navigate(`/create-club-form`);
+            }
         }).catch((error) => {
             console.error(error);
         });
     };
+    
 
     return (
         <div>

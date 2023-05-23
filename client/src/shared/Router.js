483488b4dbd879fcc3b58e789170d6306614c4dd
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-05-23 16:48:40 +0900

    Design: Design Club page
    클럽페이지추가 Router에 /club경로추가 완료.
    반복사용되는 ClubCard 컴포넌트 추가
    반복사용되는 ReviewCard 컴포넌트 추가

--- client/src/shared/Router.js
+++ client/src/shared/Router.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "../pages/Main";
 import CreateClubForm from "../pages/CreateClubForm";
+import Club from "../pages/Club";
 
 const Router = () => {
   return (
@@ -8,6 +9,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Main />}/>
         <Route path="/create-club-form" element={<CreateClubForm />}/>
+        <Route path="/club" element={<Club />}/>
       </Routes>
     </BrowserRouter>
   );

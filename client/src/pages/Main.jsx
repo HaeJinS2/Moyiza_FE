a66ce945a6353a5413464d557d9975317296bfdd
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-05-23 14:57:47 +0900

    Design: Design main page
    aos라이브러리 사용하던것 제거
    framer-motion을 사용해 애니매이션 구성
    최대 너비를 정의하기 위해 Container 컴포넌트 추가

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,62 +1,66 @@
 import React, { useEffect } from "react";
 import CreateClub from "./CreateClub";
-import AOS from "aos";
-import "aos/dist/aos.css";
 import { motion } from "framer-motion";
 import styles from "../styles";
-import { slideIn, staggerContainer, textVariant } from "../utils/motion";
+import { slideIn, staggerContainer } from "../utils/motion";
+import Navbar from "../component/Navbar";
+import Container from "../component/Container";
 
 function Main() {
-  useEffect(() => {
-    AOS.init();
-  });
-
   return (
     <>
-      <div className=" h-screen"></div>
-      <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
-        {/* 예시 1번 section */}
-        <motion.div
-          variants={staggerContainer}
-          initial="hidden"
-          whileInView="show"
-          viewport={{ once: false, amount: 0.25 }}
-          className={`${styles.innerWidth} mx-auto flex flex-col h-screen`}
-        >
-          <div className="flex justify-center items-center flex-row relative z-10 ">
-            <motion.h1
-              variants={textVariant(1.1)}
-              className={`${styles.heroHeading}`}
-            >
-              Moyiza
-            </motion.h1>
-          </div>
-          <div className="flex items-center justify-center gap-32">
-            <motion.div
-              variants={slideIn("left", "tween", 0.2, 1)}
-              className="relative w-auto"
-            >
-              <div className="w-full">
-                <div>몸과 마음이</div>
-                <div>건강해지는 습관 만들기</div>
-                <div>대한민국 1등 모임 앱, Moyiza</div>
-              </div>
-            </motion.div>
-            <motion.div
-              variants={slideIn("right", "tween", 0.2, 1)}
-              className="relative md:mt-[60px] -mt-[12px]"
-            >
-              <div className=" w-full h-[300px] justify-start items-start rounded-top-[140px] z-[0]  ">
-                <img src={`${process.env.PUBLIC_URL}/images/cat.jpeg`} alt="tmp-alt" />
-              </div>
-            </motion.div>
+      <Container>
+        <Navbar />
+        <div className=" h-screen"></div>
+        <section className={`${styles.paddings}`}>
+          <motion.div
+            variants={staggerContainer}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.25 }}
+            className={`mx-auto flex flex-col`}
+          >
+            <div className="flex items-center justify-center gap-16">
+              <motion.div
+                variants={slideIn("left", "tween", 0.2, 1)}
+                className="relative w-auto"
+              >
+                <div className="flex flex-col w-[590px] h-[341px] bg-rose-400 rounded-lg text-white text-xl justify-end items-end">
+                  <div>몸과 마음이</div>
+                  <div>건강해지는 습관 만들기</div>
+                  <div>대한민국 1등 모임 앱, Moyiza</div>
+                  <div className="flex justify-between gap-20">
+                    <div>버튼</div>
+                    <div>버튼</div>
+                  </div>
+                </div>
+              </motion.div>
+              <motion.div
+                variants={slideIn("right", "tween", 0.2, 1)}
+                className="relative md:mt-[60px] -mt-[12px]"
+              >
+                <div className=" w-full h-[498px] justify-start items-start rounded-top-[140px] z-[0]  ">
+                  <img
+                    src={`${process.env.PUBLIC_URL}/images/cat.jpeg`}
+                    alt="tmp-alt"
+                    width="590.5px"
+                    height="498px"
+                  />
+                </div>
+              </motion.div>
+            </div>
+          </motion.div>
+        </section>
+        <section className={`${styles.yPaddings} sm:pl-16 pl-6 h-screen`}>
+          <div className="flex gap-10">
+            <div className="bg-rose-400 text-white rounded-xl px-4 py-1">클럽</div>
+            <div className="bg-rose-400 text-white rounded-xl px-4 py-1">원데이</div>
           </div>
           <div>
             <CreateClub />
           </div>
-        </motion.div>
-      </section>
-      <div className=" h-screen"></div>
+        </section>
+      </Container>
     </>
   );
 }
--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -1,7 +1,4 @@
 const styles = {
-  innerWidth: "2xl:max-w-[1280px] w-full",
-  interWidth: "lg:w-[80%] w-[100%]",
-
   paddings: "sm:p-16 xs:p-8 px-6 py-12",
   yPaddings: "sm:py-16 xs:py-8 py-12",
   xPaddings: "sm:px-16 px-6",
@@ -11,11 +8,7 @@ const styles = {
   flexCenter: "flex justify-center items-center",
   flexStart: "flex justify-start items-start",
   flexEnd: "flex justify-end",
-  navPadding: "pt-[98px]",
 
-  // hero section
-  heroHeading:
-    "font-bold md:text-[100px] sm:text-[60px] text-[44px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase ",
 };
 
 export default styles;

0248202f41c1b8b4013f12377ab8e7181263275b
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-05-23 15:04:23 +0900

    Chore: Remove unused imported useEffect
    Main.jsx에서 사용하지 않는 useEffect 제거

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import CreateClub from "./CreateClub";
 import { motion } from "framer-motion";
 import styles from "../styles";

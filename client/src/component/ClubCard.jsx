3913d454c2dc4017c79ba93eed8010054f8622a7
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-05-24 23:21:49 +0900

    Feat: Add function getClubDetail to get infomation of the club
    클럽상세조회 API를 활용하여 클럽 카드를 클릭하여 상세페이지에 들어갔을 때 클럽의 상세내용을 가져옴

--- client/src/component/ClubCard.jsx
+++ client/src/component/ClubCard.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-function ClubCard({ category, title, content, thumbnail }) {
+function ClubCard({ category, title, content, thumbnail,id }) {
+  const navigate = useNavigate()
   return (
-    <div className="flex w-[588px] h-[261px] bg-slate-400">
+    <div 
+    onClick={() =>
+      navigate(`/detail/${id}`)
+    }
+    className="flex w-[588px] h-[261px] bg-slate-400">
       <div className="flex justify-around items-center">
         <div className="w-[240.5px] h-[218.5px] bg-blue-400">
           <img src={thumbnail} alt="clubThumbnail" />

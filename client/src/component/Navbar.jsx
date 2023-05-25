cd1d64f8233ad20365d4e6e5587431a7d2e1ebcb
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-05-25 10:55:31 +0900

    Design: Design navbar
    네비게이션바 디자인 수정
    로그인버튼이 가장 우측에 위치하도록 했고, 최대너비 1200px로 고정

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { staggerContainer, textVariant } from "../utils/motion";
 import { useNavigate } from "react-router-dom";
+import BodyContainer from "./BodyContainer";
 
 function Navbar() {
   const navigate = useNavigate();
@@ -9,35 +10,44 @@ function Navbar() {
   return (
     <>
       <div className="flex justify-center items-center ">
-        <div className="fixed justify-around z-20 bg-white w-full ">
-          <motion.div
-            variants={staggerContainer}
-            initial="hidden"
-            whileInView="show"
-            viewport={{ once: false, amount: 0.25 }}
-            className={`w-full flex flex-row justify-center gap-20`}
-          >
-            <div className="flex gap-10 items-end text-lg ">
-              <div className="cursor-pointer">로고</div>
-              <div onClick={() => navigate("/club")} className="cursor-pointer">
-                클럽
-              </div>
-              <div className="cursor-pointer">원데이</div>
-            </div>
-            <motion.h1
-            
-              onClick={() => navigate("/")}
-              variants={textVariant(0.5)}
-              className={`text-7xl font-bold uppercase mt-20 cursor-pointer`}
+        <div className="fixed z-20 bg-white w-full ">
+          <BodyContainer>
+            <motion.div
+              variants={staggerContainer}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: false, amount: 0.25 }}
+              className={`w-full flex flex-row justify-between gap-20 px-10`}
             >
-              Moyiza
-            </motion.h1>
-            <div className="flex gap-10 items-end text-lg">
-              <div className="cursor-pointer">검색</div>
-              <div onClick={() => navigate("/logins")} className="cursor-pointer">로그인</div>
-              <div className="cursor-pointer">프로필</div>
-            </div>
-          </motion.div>
+              <div className="flex gap-10 items-end text-lg ">
+                <div className="cursor-pointer">로고</div>
+                <div
+                  onClick={() => navigate("/club")}
+                  className="cursor-pointer"
+                >
+                  클럽
+                </div>
+                <div className="cursor-pointer">원데이</div>
+              </div>
+              <motion.h1
+                onClick={() => navigate("/")}
+                variants={textVariant(0.5)}
+                className={`text-7xl font-bold uppercase mt-20 cursor-pointer`}
+              >
+                Moyiza
+              </motion.h1>
+              <div className="flex gap-10 items-end text-lg">
+                <div className="cursor-pointer">검색</div>
+                <div className="cursor-pointer">회원가입</div>
+                <div
+                  onClick={() => navigate("/logins")}
+                  className="cursor-pointer"
+                >
+                  로그인
+                </div>
+              </div>
+            </motion.div>
+          </BodyContainer>
         </div>
       </div>
     </>

c662394581e1adec2959ff59c0fc3d971a8bc847
HaeJinS2 <9674qzq@gmail.com>
2023-05-28 01:19:51 +0900

    Feat: Add kakoo maps API in CreateEventForm

--- client/src/pages/CreateEventForm.jsx
+++ client/src/pages/CreateEventForm.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { motion } from "framer-motion";
-import { postAPI } from "../axios";
+import { postAPI, getHeaderAPI } from "../axios";
 import DatePicker from 'react-datepicker';
 import styled, { createGlobalStyle } from "styled-components";
 import Navbar from "../component/Navbar";
@@ -17,12 +17,119 @@ function CreateEvent() {
     const [startDate, setStartDate] = useState(new Date());
     const [dateString, setDateString] = useState('');
     const [eventGroupsize, setEventGroupSize] = useState('');
+    const [inputValue, setInputValue] = useState("");
+    const [map, setMap] = useState(null);
+    const [userLat, setUserLat] = useState(null);
+    const [userLng, setUserLng] = useState(null);
+    const [userAddress, setUserAddress] = useState("");
+    const [marker, setMarker] = useState(null);
     const navigate = useNavigate();
-    
+
     const { id } = useParams();
 
-    const handleCreateButton = () => {
 
+    useEffect(() => {
+        console.log(userLat, userLng, userAddress)
+    }, [userLat, userLng, userAddress])
+
+    useEffect(() => {
+        if (map && inputValue) {
+            let url = `https://dapi.kakao.com/v2/local/search/address.json?query=${inputValue}`;
+            getHeaderAPI(url, {
+                headers: { Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_MAPS_API_KEY_REST}` },
+            })
+                .then((response) => {
+                    let address = response.data.documents[0];
+                    let lat = address.y;
+                    let lng = address.x;
+
+                    let moveLatLon = new window.kakao.maps.LatLng(lat, lng);
+                    map.setCenter(moveLatLon);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+    }, [inputValue, map]);
+
+
+    useEffect(() => {
+
+        initializeMap();
+    }
+        , [])
+    //     if (modalIsOpen) {
+    //         const delay = setTimeout(initializeMap, 100);
+    //         return () => clearTimeout(delay);
+    //     }
+    // }, [modalIsOpen]);
+
+    useEffect(() => {
+        if (map) {
+            const handleMapClick = (mouseEvent) => {
+                let latlng = mouseEvent.latLng;
+                let lat = latlng.getLat();
+                let lng = latlng.getLng();
+
+                // 이전 마커가 있다면 제거
+                if (marker) {
+                    marker.setMap(null);
+                }
+
+                // 클릭한 위치에 마커 생성
+                let newMarker = new window.kakao.maps.Marker({
+                    position: new window.kakao.maps.LatLng(lat, lng)
+                });
+
+                // 마커를 지도에 표시
+                newMarker.setMap(map);
+
+                // 마커 상태 업데이트
+                setMarker(newMarker);
+
+                // 위도 경도 상태 업데이트
+                setUserLat(lat);
+                setUserLng(lng);
+
+                let url = `https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${lng}&y=${lat}&input_coord=WGS84`;
+                getHeaderAPI(url, {
+                    headers: { Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_MAPS_API_KEY_REST}` },
+                })
+                    .then((response) => {
+                        let address = response.data.documents[0].address;
+                        setUserAddress(address.address_name);
+                        console.log(response.data.documents[0])
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            };
+
+            window.kakao.maps.event.addListener(map, "click", handleMapClick);
+
+            return () => {
+                window.kakao.maps.event.removeListener(map, "click", handleMapClick);
+            };
+        }
+    }, [map, marker]);
+
+    const initializeMap = () => {
+        if (window.kakao && window.kakao.maps) {
+            let mapContainer = document.getElementById("map");
+            let mapOption = {
+                center: new window.kakao.maps.LatLng(37.5665, 126.9780),
+                level: 3,
+            };
+
+            let mapInstance = new window.kakao.maps.Map(mapContainer, mapOption);
+            setMap(mapInstance);
+        }
+    }
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const handleCreateButton = () => {
         postAPI(`/club/${id}/event`, {
             eventTitle: title, eventContent: content, eventLocation: location, eventStartTime: dateString, eventGroupSize: Number(eventGroupsize),
         }).then((response) => {
@@ -52,7 +159,7 @@ function CreateEvent() {
     return (
         <>
             <Navbar />
-            <div className='flex flex-1 flex-col items-center justify-center  h-[100vh] '>
+            <div className='mt-20 flex flex-1 flex-col items-center justify-center  h-auto '>
                 <motion.div
                     variants={fadeIn('up', 0.3)}
                     initial="hidden"
@@ -76,6 +183,13 @@ function CreateEvent() {
                             />
                         </div>
                         eventGroupsize <input className='shadow-md w-80 h-12 rounded-lg mb-4 border-1' value={eventGroupsize} onChange={(e) => setEventGroupSize(e.target.value)} />
+                        location_tmp
+                        <div className='flex flex-col gap-y-4'>
+                            <input className='w-[500px] h-[50px] shadow-md'
+                                placeholder='장소를 검색하세요 (예: xx동)'
+                                type="text" onChange={handleInputChange} />
+                            <div id="map" style={{ width: "500px", height: "400px" }}></div>
+                        </div>
                         <button onClick={handleCreateButton}>이벤트생성하기버튼</button>
                     </div>
                 </motion.div>
@@ -122,4 +236,4 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 
-export default CreateEvent
+export default CreateEvent;

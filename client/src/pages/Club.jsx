2bd3d52bbe7af3ea718a67ec6809d658ea6cc348
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-05-28 16:54:44 +0900

    Feat: Add a function that filters by category
    카테고리에 따라 필터링하는 기능 추가
    서버에서 카테고리를 받아 뿌려주는 기능 추가
    EmptyStates 컴포넌트를 이용해 검색결과가 없을 때에 클럽 개설하기 버튼이 보이게 함.
    Design: 클럽카드들이 렌더링될때 아래에서 위로 올라오게하는 애니매이션 추가

--- client/src/pages/Club.jsx
+++ client/src/pages/Club.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
+import Fade from "react-reveal/Fade";
 
 import BodyContainer from "../component/BodyContainer";
 import ClubCard from "../component/ClubCard";
@@ -12,78 +13,78 @@ import SearchBar from "../component/SearchBar";
 import { useRecoilState } from "recoil";
 import Loading from "../component/Loading";
 import { isLoadingState } from "../states/clubState";
-const tabs = [
-  "전체",
-  "문화・예술",
-  "운동・액티비티",
-  "푸드・드링크",
-  "취미",
-  "여행・동행",
-  "성장・자기계발",
-  "동네・또래",
-  "연애・소개팅",
-];
+import EmptyState from "../component/EmptyState";
 
 function Club() {
-  const [activeTab, setActiveTab] = useState(tabs[0]);
+  const [activeTab, setActiveTab] = useState("전체");
   const [page, setPage] = useState(0);
   const [club, setClub] = useState([]);
   const [totalPages, setTotalPages] = useState(0);
   const [search, setSearch] = useState("");
   const [isLoading, setIsLoading] = useRecoilState(isLoadingState);
+  const [categories, setCategories] = useState(null);
+  const [filteredClubList, setFilteredClubList] = useState([]);
   const divRef = useRef(null);
 
   useEffect(() => {
     divRef.current.scrollIntoView({ behavior: "smooth" });
   }, []);
 
-  //클럽 목록을 받아오는 코드
   useEffect(() => {
-    setIsLoading(true)
+    setIsLoading(true);
+    // 클럽 목록을 받아오는 코드
     getAPI(`/club?page=${page}&size=8&sort=createdAt,DESC`).then((res) => {
       setClub([...club, ...res.data.content]);
+      setFilteredClubList([...club, ...res.data.content])
     });
-    getAPI("/club").then((res) => setTotalPages(res.data.totalPages));
-    setIsLoading(false)
 
+    // 클럽 전체 페이지를 가져오는 코드
+    getAPI("/club").then((res) => setTotalPages(res.data.totalPages));
+    setIsLoading(false);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page]);
 
+  useEffect(() => {
+    // 클럽 카테고리를 가져오는 코드
+    getAPI(`/enums`).then((res) => {
+      const newCategorylist = ["전체", ...res.data.categoryList];
+      setCategories(newCategorylist);
+    });
+  }, []);
+
   //카테고리에 따라 검색하는 코드
   const handleClubCategory = (e) => {
-    console.log(e.target.innerHTML);
-    getAPI(`/club/search?q=${search}&category=여행`)
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
+    if(e.target.innerHTML === "전체") {
+      setFilteredClubList(club)
+    } else {
+      getAPI(`/club/search?q=${search}&category=${e.target.innerHTML}`)
+      .then((res) => setFilteredClubList(res.data.content))
+      .catch((err) => setFilteredClubList([]));
+    } 
   };
 
   const handleSearchInput = (e) => {
-    setSearch(e.target.change);
+    setSearch(e.target.value);
   };
 
-  console.log(club);
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <div ref={divRef} >
+      <div ref={divRef}>
         <Container>
           <Navbar />
           <section className="h-auto mt-24 mb-10">
             <BodyContainer>
               <body className="flex flex-col">
-                {/* <div className="flex justify-end py-4">
-         <button className="bg-rose-400 text-white rounded-lg px-2 py-1">
-           필터
-         </button>
-       </div> */}
                 <SearchBar
                   handleSearchInput={handleSearchInput}
                   search={search}
                 />
                 <div className="flex justify-around  my-4">
-                  {tabs.map((tab, i) => (
+                  {categories?.map((tab, i) => (
                     <button
                       key={i}
                       onClick={(e) => {
@@ -111,24 +112,51 @@ function Club() {
                   ))}
                 </div>
                 <div className="flex flex-col justify-between">
-                  <div className="grid grid-cols-2 gap-x-4 gap-y-8">
-                    {club?.map((item, i) => {
-                      return (
-                        <ClubCard
-                          key={i}
-                          title={item.clubTitle}
-                          content={item.clubContent}
-                          tag={item.clubTag}
-                          thumbnail={item.thumbnailUrl}
-                          id={item.club_id}
-                          maxGroupSize={item.maxGroupSize}
-                          nowMemberCount={item.nowMemberCount}
-                        />
-                      );
-                    })}
+                  <div
+                    className={`grid ${
+                      filteredClubList.length === 0 ? "" : "grid-cols-2"
+                    }  gap-x-4 gap-y-8`}
+                  >
+                    {filteredClubList.length === 0 ? (
+                      <EmptyState showReset handleClubCategory={handleClubCategory} />
+                    ) : filteredClubList ? (
+                      filteredClubList?.map((item, i) => {
+                        return (
+                          <Fade bottom>
+                            <ClubCard
+                              key={i}
+                              title={item.clubTitle}
+                              content={item.clubContent}
+                              tag={item.clubTag}
+                              thumbnail={item.thumbnailUrl}
+                              id={item.club_id}
+                              maxGroupSize={item.maxGroupSize}
+                              nowMemberCount={item.nowMemberCount}
+                            />
+                          </Fade>
+                        );
+                      })
+                    ) : (
+                      club?.map((item, i) => {
+                        return (
+                          <Fade bottom>
+                            <ClubCard
+                              key={i}
+                              title={item.clubTitle}
+                              content={item.clubContent}
+                              tag={item.clubTag}
+                              thumbnail={item.thumbnailUrl}
+                              id={item.club_id}
+                              maxGroupSize={item.maxGroupSize}
+                              nowMemberCount={item.nowMemberCount}
+                            />
+                          </Fade>
+                        );
+                      })
+                    )}
                   </div>
                 </div>
-                {totalPages > page + 1 && (
+                {filteredClubList.length >= 8 && totalPages > page + 1 && (
                   <div className="flex justify-center mt-10">
                     <button
                       onClick={() => setPage(page + 1)}
@@ -143,17 +171,6 @@ function Club() {
           </section>
           <section className="h-auto mb-10">
             <BodyContainer>
-              {/* <div>
-       <p>후기</p>
-     </div>
-     <div className="flex flex-1 justify-around">
-       <div className="grid grid-cols-2 gap-x-4 gap-y-8">
-         <ReviewCard />
-         <ReviewCard />
-         <ReviewCard />
-         <ReviewCard />
-       </div>
-     </div> */}
               <div className="flex justify-end">
                 <div className="fixed z-100 bottom-16 flex justify-center items-center mt-10 bg-rose-400 text-white w-[130px] py-2 rounded-lg">
                   <CreateClub />
@@ -162,8 +179,7 @@ function Club() {
             </BodyContainer>
           </section>
         </Container>
-        </div>
-      )}
+      </div>
     </>
   );
 }

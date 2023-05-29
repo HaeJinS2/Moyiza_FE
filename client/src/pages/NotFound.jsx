128b2d3771a1c489697aa3557a0a1f0c6548e7d9
HaeJinS2 <9674qzq@gmail.com>
2023-05-29 14:12:13 +0900

    Design: Add 404NotFound page

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { FaExclamationTriangle } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 
 function NotFound() {
+  const navigate = useNavigate();
   return (
-    <div>404</div>
+    <div className='flex flex-col items-center justify-center h-[100vh]'>
+      <div className='flex text-7xl font-bold mb-2'>
+      <h1 className='flex gap-2'>
+        <FaExclamationTriangle /> <span>404</span>
+      </h1>
+      </div>
+      <h4>Sorry, there is nothing here</h4>
+      <button 
+      className='text-slate-600'
+      onClick={()=>navigate(-2)}>Go Back</button>
+    </div>
   )
 }
 

0baee4cec0858262136c5d21a2be062ec33cb576
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-06-03 00:06:12 +0900

    Design: Design navbar and main page and searchbar
    네비게이션 바, 메인페이지, 검색바 디자인

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
+import SearchBar from "./SearchBar";
 
 function Navbar() {
   const [isLoggedIn, setIsLoggedIn] = useState(null); // 로그인 상태 여부를 관리할 상태값 추가
@@ -8,17 +9,17 @@ function Navbar() {
   useEffect(() => {
     console.log("isLoggedIn", isLoggedIn);
     // 로그인 여부를 확인하고 상태값 업데이트
-    const cookie = Cookies.get('ACCESS_TOKEN');
+    const cookie = Cookies.get("ACCESS_TOKEN");
     setIsLoggedIn(cookie ? true : false);
   }, [isLoggedIn]);
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
     // console.log(Cookies)
-    Cookies.remove('REFRESH_TOKEN');
-    Cookies.remove('ACCESS_TOKEN');
-    navigate('/');
-    alert('로그아웃 되었습니다.')
+    Cookies.remove("REFRESH_TOKEN");
+    Cookies.remove("ACCESS_TOKEN");
+    navigate("/");
+    alert("로그아웃 되었습니다.");
   };
 
   // const goHome = () => {
@@ -26,10 +27,9 @@ function Navbar() {
   // }
 
   const goMyInfo = () => {
-    navigate('/user/profile')
-  }
+    navigate("/user/profile");
+  };
 
-  
   const [isScrolled, setIsScrolled] = useState(false);
 
   const checkScroll = () => {
@@ -49,78 +49,61 @@ function Navbar() {
 
   return (
     <>
-      <div className="flex justify-center">
-        <div className="fixed z-20">
-          {/* <BodyContainer> */}
-          {/* <motion.div
-              variants={staggerContainer}
-              initial="hidden"
-              whileInView="show"
-              viewport={{ once: false, amount: 0.25 }}
-              className={`w-full flex flex-row justify-between gap-20 `}
-            > */}
-          <div
-            className={`flex justify-center bg-white w-[100vw]  transition-all duration-200 ease-in-out 
+      <div className="fixed z-20">
+        <div
+          className={`flex justify-center bg-white w-[100vw]  transition-all duration-200 ease-in-out 
       ${isScrolled ? " border-b-[1px] border-gray-300" : ""}`}
+        >
+          <div
+            className={`flex w-[1200px] justify-between items-center transition-all duration-200 ease-in-out  ${
+              isScrolled ? "h-[100px]" : "h-[140px]"
+            }`}
           >
-            <div
-              className={`flex w-[1000px] justify-between items-center transition-all duration-200 ease-in-out  ${
-                isScrolled ? "h-[65px]" : "h-[80px]"
-              }`}
-            >
-              <div className="w-full flex justify-between items-center ">
-                <div className="flex justify-between items-center text-lg gap-x-2 ml-[-10px]">
-                  <img
-                    className="cursor-pointer ml-[10px]"
-                    onClick={() => navigate("/")}
-                    src={`${process.env.PUBLIC_URL}/images/logo.svg`}
-                    width="100px"
-                    alt="moyizaLogo"
-                  />
-                </div>
-                {/* <motion.h1
-                onClick={() => navigate("/")}
-                variants={textVariant(0.5)}
-                className={`text-5xl font-bold uppercase mt-20 cursor-pointer `}
-              > */}
-                {/* <div
+            <div className="w-full flex justify-between items-center ">
+              <div className="flex justify-between items-center text-lg gap-x-2 ml-[-10px]">
+                <img
+                  className="cursor-pointer ml-[10px]"
                   onClick={() => navigate("/")}
-                  className="text-5xl flex items-center font-bold uppercase  cursor-pointer">
-                  Moyiza
-                </div> */}
-                {/* </motion.h1> */}
-                <div className="flex items-center text-md font-semibold gap-x-8">
-                  <div
+                  src={`${process.env.PUBLIC_URL}/images/logo.svg`}
+                  width="140px"
+                  alt="moyizaLogo"
+                />
+              </div>
 
-                    onClick={() => navigate("/chat")}
-                    className="cursor-pointer"
-                  >채팅</div>
-                  {isLoggedIn ? 
-                  ( //로그인 상태인 경우
-                    <>
-                    <div onClick={goMyInfo} className="cursor-pointer">프로필</div>
-                    <div onClick={logoutHandler} className="cursor-pointer">로그아웃</div>
-                    </> 
-                    )
-                    : ( //로그인 상태 아닌 경우
-                      <>
-                    <div onClick={() => navigate("/signup")} className="cursor-pointer">회원가입</div>
-                    <div onClick={() => navigate("/logins")} className="cursor-pointer">로그인</div>
+              <SearchBar />
+              <div className="w-[140px] flex items-center text-md font-semibold justify-center">
+                {isLoggedIn ? (
+                  //로그인 상태인 경우
+                  <>
+                    <div
+                      onClick={() => navigate("/chat")}
+                      className="cursor-pointer"
+                    >
+                      채팅
+                    </div>
+                    <div onClick={logoutHandler} className="cursor-pointer">
+                      알림
+                    </div>
+                    <div onClick={goMyInfo} className="cursor-pointer">
+                      프로필
+                    </div>
                   </>
-                  )}
-                   {/* <div onClick={() => navigate("/signup")} className="cursor-pointer">회원가입</div>
-                  <div
-                    onClick={() => navigate("/logins")}
-                    className="cursor-pointer"
-                  >
-                    로그인
-                  </div> */}
-                </div>
-                {/* </motion.div> */}
+                ) : (
+                  //로그인 상태 아닌 경우
+                  <>
+                    <div></div>
+                    <div></div>
+                    <div
+                      onClick={() => navigate("/logins")}
+                      className="flex cursor-pointer w-[70px] h-[60px] items-center"
+                    >
+                      로그인
+                    </div>
+                  </>
+                )}
               </div>
             </div>
           </div>
-          {/* </BodyContainer> */}
         </div>
       </div>
     </>

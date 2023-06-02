d6785c27597a93837ea0ecc3527dfce3e5ce9a04
HaeJinS2 <9674qzq@gmail.com>
2023-06-03 03:32:04 +0900

    Design: Modify Step5

--- client/src/component/createclubform/Steps.jsx
+++ client/src/component/createclubform/Steps.jsx
@@ -137,21 +137,29 @@ export const Step4 = ({ nextStep, prevStep, progress, preview, handleFileChange,
                                     {preview && (
                                         <img className="w-[70px] h-[70px]" src={preview} alt="preview" />
                                     )}
-                    <input
-                        type="file"
-                        id="fileInput"
-                        onChange={handleFileChange}
-                    />
+                                    <input
+                                        type="file"
+                                        id="fileInput"
+                                        onChange={handleFileChange}
+                                    />
+                                </div>
+                            </div>
+                            <div className="flex flex-col  gap-y-2">
+                                <div className="flex  justify-start items-start w-full">
+                                    <span className="text-[20px] text-left">2) 일상속 소개 내용을 입력하세요.</span>
+                                </div>
+                                <input
+                                    className='shadow-cms w-[954px] h-[192px] rounded-3xl mb-4 border-2 px-2'
+                                    placeholder="우리 일상을 소개해주세요."
+                                    type="text" value={contentInput} onChange={handleContentChange} />
+                            </div>
 
-                </div>
-                <div className="border w-full mb-4 mt-4"></div>
-                <input
-                    className='shadow-md w-[1200px] h-32 rounded-lg mb-4 border-2 px-2'
-                    placeholder="클럽내용을 입력하세요"
-                    type="text" value={contentInput} onChange={handleContentChange} />
-                <div className="flex items-center justify-center gap-x-4">
-                    <button className=" w-[600px] h-32 bg-[#FB7185]" onClick={prevStep}>이전</button>
-                    <button className=" w-[600px] h-32 bg-[#FB7185]" onClick={nextStep}>다음</button>
+                            <div className="flex items-center justify-center gap-x-4">
+                                <button className="w-[14rem] h-[3.8rem] bg-[#747474] text-white rounded-3xl" onClick={prevStep}>이전</button>
+                                <button className="w-[14rem] h-[3.8rem] bg-[#FF7F1E] text-white rounded-3xl" onClick={nextStep}>다음</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
         </Container>
@@ -162,47 +170,73 @@ export const Step4 = ({ nextStep, prevStep, progress, preview, handleFileChange,
 export const Step5 = ({ nextStep, prevStep, progress, selectedGenderPolicy, setSelectedGenderPolicy, option, handleAgePolicyChange, agePolicy, handleRestrictionChange, handleRestrictionChange2, restrictionInput, restrictionInput2 }) => {
     return (
         <Container>
-            <section className="h-[100vh] flex flex-1 flex-col items-center justify-center">
-                <div className="flex flex-col gap-y-10">
-                    {/* <input type="text" value={restrictionInput} onChange={handleRestrictionChange} /> */}
-                    {/* <input type="text" value={restrictionInput2} onChange={handleRestrictionChange2} /> */}
-                    <div className="grid grid-cols-4 gap-4 w-[1200px] h-32 mb-4">
-                        {option.genderPolicyLists.map((category, index) => (
-                            <button
-                                key={index}
-                                onClick={() => setSelectedGenderPolicy(category)}
-                                className={`${selectedGenderPolicy === category ? 'bg-[#b54e5d] text-white w-full h-32' : 'bg-[#FB7185] text-white w-full h-32'}`}
-                            >
-                                {category}
-                            </button>
-                        ))}
+            <section className=" h-[calc(100vh-0px)] flex flex-col items-center ">
+                <div className="flex flex-col">
+                    <div className="flex  w-full h-[500px] items-center justify-center ">
+                        <span className="text-[30px]">모두와 함께할 <span className="text-[#08B159]">일상</span>을 만들어보세요!</span>
                     </div>
-                    <div>
-                        <div>{'몇살부터?: ' + agePolicy.x}</div>
-                        <div className="w-[1200px]">
-                            {agePolicy && (
-                                <Slider
-                                    styles={{
-                                        track: {
-                                            width: 1200,
-                                        },
-                                        active: {
-                                            backgroundColor: '#FB7185'
-                                        },
-                                    }}
-                                    axis="x"
-                                    xstep={5}
-                                    xmin={20}
-                                    xmax={50}
-                                    x={agePolicy.x}
-                                    onChange={({ x }) => handleAgePolicyChange({ x: parseFloat(x.toFixed(2)) })}
-                                />
-                            )}
+                    <div className="flex flex-col items-center w-full md:w-[1920px] shadow-cm  bg-[#FFFCF2] rounded-t-[100px]">
+                        <div className="flex w-[800px] py-[111px] flex-col gap-y-24 z-10">
+                            <div className="flex justify-start items-start w-full">
+                                <span className="text-[20px] text-left">5. 함께할 일상속 멤버 조건을 설정하세요.</span>
+                            </div>
+                            <div className="flex flex-col gap-y-2">
+                                <div className="flex  justify-start items-start w-full">
+                                    <span className="text-[20px] text-left">1) 성별</span>
+                                </div>
+                                <div className="grid grid-cols-3 gap-x-[120px] gap-y-[59px] w-auto h-auto mb-4">
+                                    {option.genderPolicyLists.map((category, index) => (
+                                        <button
+                                            key={index}
+                                            onClick={() => setSelectedGenderPolicy(category)}
+                                            className={`${selectedGenderPolicy === category ? 'shadow-cms bg-[#dddddd] w-[11rem] h-[3.7rem] rounded-3xl' : 'shadow-cms bg-white w-[11rem] h-[3.7rem] rounded-3xl'}`}
+                                        >
+                                            {category}
+                                        </button>
+                                    ))}
+                                </div>
+                            </div>
+                            <div className="flex flex-col gap-y-2">
+                                <div className="flex  justify-start items-start w-full">
+                                    <span className="text-[20px] text-left">2) 연령</span>
+                                </div>
+                                <div>
+                                    <div className="w-[1000px]">
+                                        {agePolicy && (
+                                            <Slider
+                                                styles={{
+                                                    track: {
+                                                        width: 1000,
+                                                    },
+                                                    active: {
+                                                        backgroundColor: '#FFE14F'
+                                                    },
+                                                }}
+                                                axis="x"
+                                                xstep={5}
+                                                xmin={20}
+                                                xmax={50}
+                                                x={agePolicy.x}
+                                                onChange={({ x }) => handleAgePolicyChange({ x: parseFloat(x.toFixed(2)) })}
+                                            />
+                                        )}
+                                        <div className="flex justify-between">
+                                            <div>20</div>
+                                            <div>25</div>
+                                            <div>30</div>
+                                            <div>35</div>
+                                            <div>40</div>
+                                            <div>45</div>
+                                            <div>50+</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex items-center justify-center gap-4">
+                                <button className="w-[14rem] h-[3.8rem] bg-[#747474] text-white rounded-3xl" onClick={prevStep}>이전</button>
+                                <button className="w-[14rem] h-[3.8rem] bg-[#FF7F1E] text-white rounded-3xl" onClick={nextStep}>다음</button>
+                            </div>
                         </div>
-                    </div>
-                    <div className="flex items-center justify-center gap-4">
-                        <button className=" w-[600px] h-32 bg-[#FB7185]" onClick={prevStep}>이전</button>
-                        <button className=" w-[600px] h-32 bg-[#FB7185]" onClick={nextStep}>다음</button>
                     </div>
                 </div>
             </section>

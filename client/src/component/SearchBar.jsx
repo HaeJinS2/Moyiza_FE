5269792161fff7473237de66140350d1c26dabac
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-06-05 14:13:56 +0900

    Modify: Modify search bar height

--- client/src/component/SearchBar.jsx
+++ client/src/component/SearchBar.jsx
@@ -17,7 +17,7 @@ const SearchBar = ({ handleSearchInput, search, page }) => {
   }, []);
 
   return (
-    <div className="border-[1px] flex border-orange-500 w-[480px] h-[60px] items-center rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
+    <div className="border-[1px] flex border-orange-500 w-[480px] h-[50px] items-center rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
       <div className="w-full flex justify-between pr-6 pl-6 font-sans">
         <input
           onChange={handleSearchInput}

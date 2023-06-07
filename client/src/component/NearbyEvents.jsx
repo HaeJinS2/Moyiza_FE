33c55db804fa16adf581e51a1f8e533cea0784a8
HaeJinS2 <9674qzq@gmail.com>
2023-06-07 23:18:02 +0900

    Design: Add css to NearbyEvents

--- client/src/component/NearbyEvents.jsx
+++ client/src/component/NearbyEvents.jsx
@@ -3,7 +3,8 @@ import { useState } from 'react';
 import Modal from "react-modal";
 import useCurrentLocation from '../hooks/useCurrentLocation';
 import { getAPI } from '../axios';
-
+import { useRecoilValue} from "recoil";
+import { userNicknameState } from "../states/userStateTmp";
 function NearbyEvents() {
     const [isOpen, setIsOpen] = useState(false);
     const [loading, setLoading] = useState(false);
@@ -11,6 +12,7 @@ function NearbyEvents() {
     const [dataLat, setDataLat] = useState([]);
     const [dataLng, setDataLng] = useState([]);
     const [distances, setDistances] = useState([]);
+    const nicknameState = useRecoilValue(userNicknameState);
 
     console.log(data)
     console.log("받은 위치", dataLat, dataLng)
@@ -72,43 +74,20 @@ function NearbyEvents() {
         }
     }, [data]);
 
-    useEffect(() => {
-        if (location?.latitude && location?.longitude) {
-            const myLatitude = location.latitude
-            const myLongitude = location.longitude
-
-            const distances = []; // distances 배열 초기화
-
-            for (let i = 0; i < dataLat.length; i++) {
-                const distance = calculateDistance(myLatitude, myLongitude, dataLat[i], dataLng[i]);
-                distances.push(distance);
-            }
-
-            // 거리 데이터를 상태로 업데이트
-            setDistances(distances);
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [dataLat, dataLng, location]);
-
-    function calculateDistance(lat1, lon1, lat2, lon2) {
-        const R = 6371; // 지구의 반지름 (단위: km)
-
-        const dLat = toRadians(lat2 - lat1);
-        const dLon = toRadians(lon2 - lon1);
-
-        const a =
-            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        const distance = R * c;
-
-        return distance * 1000;
-    }
-
-    function toRadians(degrees) {
-        return degrees * (Math.PI / 180);
-    }
+    function formatDate(dateTimeString) {
+        const dateObj = new Date(dateTimeString);
+        const formattedDate = dateObj.toLocaleDateString('ko-KR', {
+        //   year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+          hour: 'numeric',
+        //   minute: 'numeric',
+          hour12: true,
+        });
+        
+        return formattedDate;
+      }
+      
 
     return (
         <div>
@@ -126,14 +105,17 @@ function NearbyEvents() {
                     },
                     content: {
                         color: "#000000",
-                        width: "550px",
-                        height: "800px",
+                        background: "#08B159",
+                        width: "642px",
+                        maxHeight: "815px",
                         margin: "auto",
                         display: "flex",
                         flexDirection: "column",
-                        padding: "20px",
-                        borderRadius: "10px",
-                        gap: "10px",
+                        borderRadius: "20px",
+                        // gap: "10px",
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        overflow: 'hidden',
                     },
                 }}
             >
@@ -142,19 +124,108 @@ function NearbyEvents() {
                         location ? (
                             <>
                                 <div>
-                                    {location.latitude} {location.longitude}
+                                    {/* {location.latitude} {location.longitude} */}
                                 </div>
                                 {data.map((d) => {
+                                    const formattedDate = formatDate(d.oneDay.oneDayStartTime);
                                     return (
-                                        <div>
-                                            제목: {d?.oneDay?.oneDayTitle}
-                                            내용: {d.oneDay.oneDayContent}
-                                            카테고리: {d.oneDay.category}
-                                            그룹사이즈: {d.oneDay.size}
-                                            이미지: 이미지~
-                                            주소: 주소~
-                                            시작일: {d.oneDay.oneDayStartTime}
-                                            거리: {(d.distance*1000).toFixed(1)}M
+                                        <div className='overflow-auto h-[815px] w-[642px] py-6 flex justify-center items-center '>
+                                            <div className='flex flex-col justify-center items-center gap-y-4'>
+                                                <div className='text-white font-semibold text-[30px] flex flex-col items-center '>
+                                                    <span>{nicknameState.userNickname}님 근처에</span>
+                                                    <span>모임이 진행되고있어요!</span>
+                                                </div>
+                                                <div className='w-[470px] h-[25px] flex justify-end text-white'>
+                                                    <span>총 {data.length}개</span>
+                                                </div>
+                                                <div className='w-[470px] h-[114px] bg-white rounded-[20px] gap-x-4 flex justify-center items-center'>
+                                                    <div className='w-[80px] h-[80px] bg-[#797979] rounded-[20px]'>
+                                                        <img src={d?.oneDay?.oneDayImage} alt="onedayImg">
+                                                        </img>
+                                                    </div>
+                                                    <div className='w-[330px] h-[80px] flex flex-col justify-between items-center'>
+                                                        <div className='w-full h-[25px]   flex justify-between'>
+                                                            <span className='text-[24px]'>
+                                                            {d?.oneDay?.oneDayTitle}
+                                                            </span>
+                                                            <div className='px-2 w-auto border border-[#08B159] rounded-[20px] bg-white text-[#08B159]'>
+                                                            {d.oneDay.category}
+                                                            </div>
+                                                        </div>
+                                                        <div className='w-full h-[25px] flex justify-between text-[19px] text-[#A4A4A4]'>
+                                                            <div>{formattedDate}</div>
+                                                            <div>{d.oneDay.attendantsNum}/{d.oneDay.oneDayGroupSize}</div>
+                                                            <div>{(d.distance * 1000).toFixed(1)}M</div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                {/* 제목: {d?.oneDay?.oneDayTitle}
+                                                내용: {d.oneDay.oneDayContent}
+                                                카테고리: {d.oneDay.category}
+                                                그룹사이즈: {d.oneDay.size}
+                                                이미지: 이미지~
+                                                주소: 주소~
+                                                시작일: {d.oneDay.oneDayStartTime}
+                                                거리: {(d.distance * 1000).toFixed(1)}M */}
+                                                {/* <div className='w-[470px] h-[114px] bg-white rounded-[20px] gap-x-4 flex justify-center items-center'>
+                                                    <div className='w-[80px] h-[80px] bg-[#797979] rounded-[20px]'></div>
+                                                    <div className='w-[330px] h-[80px] flex flex-col justify-between items-center'>
+                                                        <div className='w-full h-[25px]   flex justify-between'>
+                                                            <span className='text-[24px]'>
+                                                                러닝 할 사람
+                                                            </span>
+                                                            <div>
+                                                                운동
+                                                            </div>
+                                                        </div>
+                                                        <div className='w-full h-[25px] flex justify-between text-[19px] text-[#A4A4A4]'>
+                                                            <div>1</div>
+                                                            <div>2</div>
+                                                            <div>3</div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className='w-[470px] h-[114px] bg-white rounded-[20px] gap-x-4 flex justify-center items-center'>
+                                                    <div className='w-[80px] h-[80px] bg-[#797979] rounded-[20px]'></div>
+                                                    <div className='w-[330px] h-[80px] flex flex-col justify-between items-center'>
+                                                        <div className='w-full h-[25px]   flex justify-between'>
+                                                            <span className='text-[24px]'>
+                                                                러닝 할 사람
+                                                            </span>
+                                                            <div>
+                                                                운동
+                                                            </div>
+                                                        </div>
+                                                        <div className='w-full h-[25px] flex justify-between text-[19px] text-[#A4A4A4]'>
+                                                            <div>1</div>
+                                                            <div>2</div>
+                                                            <div>3</div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className='w-[470px] h-[114px] bg-white rounded-[20px] gap-x-4 flex justify-center items-center'>
+                                                    <div className='w-[80px] h-[80px] bg-[#797979] rounded-[20px]'></div>
+                                                    <div className='w-[330px] h-[80px] flex flex-col justify-between items-center'>
+                                                        <div className='w-full h-[25px]   flex justify-between'>
+                                                            <span className='text-[24px]'>
+                                                                러닝 할 사람
+                                                            </span>
+                                                            <div>
+                                                                운동
+                                                            </div>
+                                                        </div>
+                                                        <div className='w-full h-[25px] flex justify-between text-[19px] text-[#A4A4A4]'>
+                                                            <div>1</div>
+                                                            <div>2</div>
+                                                            <div>3</div>
+                                                        </div>
+                                                    </div>
+                                                </div> */}
+                                                <button 
+                                                onClick={closeModal}
+                                                className='flex justify-center items-center w-[50px] h-[50px] bg-white shadow-cms rounded-full'>X</button>
+
+                                            </div>
                                         </div>
                                     );
                                 })}

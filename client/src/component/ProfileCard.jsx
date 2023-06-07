6e7df1e3c499455cd1c24c297d83ddcceaa28fcc
minnjeong <bbono4500@gmail.com>
2023-06-07 23:54:43 +0900

    Feat:Modify profile image, Add MyInfoClub

--- client/src/component/ProfileCard.jsx
+++ client/src/component/ProfileCard.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function ProfileCard({
-    tag,
-    title,
-    content,
-    thumbnail,
-    id,
+    clubTitle,
+    thumbnailUrl,
+    club_id,
     maxGroupSize,
     nowMemberCount,
-    location
+    clubContent,
+    clubTag
 
 }) {
     const navigate = useNavigate();
@@ -17,49 +16,41 @@ function ProfileCard({
         <>
 
             <div
-                onClick={() => navigate(`/detail/${id}`)}
+                onClick={() => navigate(`/detail/${club_id}`)}
                 className="cursor-pointer flex w-[1200px] shadow-cm rounded-xl h-[220px] items-center justify-center px-2 mt-10 bg-white"
             >
                 <div className="flex items-center rounded-xl ">
                     <img
                         className="rounded-md w-[258px] h-[178px] border-[1.5px] ml-5"
-                        src={thumbnail}
+                        src={thumbnailUrl}
                         alt="clubThumbnail"
                     />
                 </div>
-                {/* <div className="flex flex-col gap-4 w-full h-full pl-4 pr-2 py-6"> */}
-                {/* <div className="flex h-full justify-between flex-col"> */}
                 <div className="w-[816px] h-[178px] ml-[35px] mt-[16px] flex flex-col">
                     <div className="flex justify-between">
                         <div className="text-[28px] font-bold">
-                            {/* {title} */}
-                            화곡역에서 러닝할 사람
+                            {clubTitle}
                         </div>
                         <div className="text-[24px]">
-                            {/* {nowMemberCount} / {maxGroupSize} */}
-                            12 / 20
+                            {nowMemberCount} / {maxGroupSize}
                         </div>
                     </div>
 
                     <div className="text-[16px] text-[#686868] mt-[16px] w-[450px] h-[77px]">
-                        {/* {content} */}
-                        매주 수요일 7시 화곡역에 만나서 러닝해요! 운동목적모임입니다!
+                        {clubContent}
+                        {/* 매주 수요일 7시 화곡역에 만나서 러닝해요! 운동목적모임입니다! */}
                     </div>
-                    <div className="text-[16px] text-orange-400 mt-[20px] mb-[10px] w-[813px] h-[31px] flex justify-between">
-                        <div className="rounded-full b-1 border-2 px-2 py-1 border-orange-400 ">
-                            {/* {tag} */}
-                            스포츠
-                        </div>
-                        <div className="text-black text-[24px] ">
-                            {/* {location} */}
-                            화곡역
-                        </div>
+                    <div className="text-[16px] text-orange-400 mt-[20px] mb-[10px] w-[813px] h-[31px] flex">
+                        {clubTag.map((tag) => {
+                            return (
+                                <div className="rounded-full mr-2 b-1 border-2 px-2 py-1 border-orange-400 flex justify-start">
+                                    {tag}
+                                </div>
+                            );
+                        })}
                     </div>
                 </div>
             </div>
-            {/* </div> */}
-            {/* </div> */}
-
         </>
     );
 }
--- client/src/component/UserProfile.jsx
+++ client/src/component/UserProfile.jsx
@@ -3,91 +3,98 @@ import Modal from 'react-modal';
 import Frame from '../component/img/Frame.png';
 
 
-// 모달 스타일 설정
-const modalStyles = {
-    // content: `w-[600px] h-[500px] mx-auto border-2 border-gray-300 rounded-lg p-6'`
-    content: {
-        width: '500px',
-        height: '500px',
-        margin: 'auto',
-        border: '1px solid #ccc',
-        borderRadius: '50px',
-        padding: '30px',
-    },
-};
+function UserProfile({
+    userInfo
+}) {
 
-// 프로필 페이지 컴포넌트
-const UserProfile = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
 
-    // 모달 열기
-    const openModal = () => {
-        setIsModalOpen(true);
+    // const {nickname, profileImage} = userInfo;
+    // 모달 스타일 설정
+    const modalStyles = {
+        // content: `w-[600px] h-[500px] mx-auto border-2 border-gray-300 rounded-lg p-6'`
+        content: {
+            width: '500px',
+            height: '500px',
+            margin: 'auto',
+            border: '1px solid #ccc',
+            borderRadius: '50px',
+            padding: '30px',
+        },
     };
 
-    // 모달 닫기
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
+    // 프로필 페이지 컴포넌트
+    
+        const [isModalOpen, setIsModalOpen] = useState(false);
 
-    return (
-        <div>
-            {/* 프로필 사진 */}
-            <div style={{ alignItems: 'center' }} className="profile-picture mt-[240px] mb-[114px] flex flex-col justify-center">
-                <div className="w-[200px] h-[200px] bg-gray-400 rounded-full drop-shadow-md"
-                //  src={google} alt="Profile"
-                />
-                {/* 작은 원 */}
-                <div className='absolute right-[45%]'>
-                    <div className="edit-icon w-[56px] h-[56px] bg-[#FFFCF2] shadow hover:shadow-lg rounded-full flex items-center justify-center" onClick={openModal}>
-                        <img src={Frame} alt=''></img>
-                    </div>
-                </div>
+        // 모달 열기
+        const openModal = () => {
+            setIsModalOpen(true);
+        };
 
-                <div>
-                    <div className='name mt-[16px] text-[34px] flex items-center justify-center'>김세영</div>
-                    <div className='email mt-[7px] text-[24px] text-[#A6A6A6]'>bobo@naver.com</div>
-                </div>
-                <div className="text-[16px]  mt-[17px] flex justify-between">
-                    <div className="rounded-full bg-[#FFE14F] b-1 px-4 py-1 text-[white]">
-                        {/* {tag} */}
-                        필라테스
-                    </div>
-                </div>
-            </div>
+        // 모달 닫기
+        const closeModal = () => {
+            setIsModalOpen(false);
+        };
 
-            {/* 프로필 수정 모달 */}
-            <Modal
-                isOpen={isModalOpen}
-                onRequestClose={closeModal}
-                style={modalStyles}
-                contentLabel="Profile Modal"
-            >
-                <div className='flex flex-col items-center'>
-                    <h2 className='mb-10 text-[20px] font-medium'>프로필 수정</h2>
-                    <div className='flex items-center mb-5 w-full'>
-                        <div className='w-[30%]'>
-                            <p className='font-medium'>이름</p></div>
-                        <input placeholder='김세영' class="int w-[70%] h-10 rounded-lg px-3.5 py-2 shadow"></input>
-                    </div>
-                    <div className='flex items-center mb-5 w-full'>
-                        {/* 고정값 */}
-                        <div className='w-[30%]'>
-                            <p className='font-medium'>이메일</p></div>
-                        <div class="int h-10 rounded-lg px-3.5 py-2 w-[70%] shadow">bobo@naver.com</div>
-                    </div>
-                    <div className='flex items-center mb-[180px] w-full'>
-                        <div className='font-medium'>관심사</div>
-                        <div>
 
+        return (
+            <div>
+                {/* 프로필 사진 */}
+                <div style={{ alignItems: 'center' }} className="profile-picture mt-[240px] mb-[114px] flex flex-col justify-center">
+                    <div className="w-[200px] h-[200px] bg-gray-400 rounded-full drop-shadow-md"
+                    //  src={profileImage} alt="Profile"
+                    />
+                    {/* 작은 원 */}
+                    <div className='absolute right-[45%]'>
+                        <div className="edit-icon w-[56px] h-[56px] bg-[#FFFCF2] shadow hover:shadow-lg rounded-full flex items-center justify-center" onClick={openModal}>
+                            <img src={Frame} alt=''></img>
                         </div>
                     </div>
-                    <button onClick={closeModal} className='bg-[#FF7F1E] text-white rounded-[50px] w-20 h-10 mb-[10px] shadow hover:shadow-lg'>저장</button>
+
+                    <div>
+                        {/* <div className='name mt-[16px] text-[34px] flex items-center justify-center'>{nickname}</div> */}
+                        <div className='email mt-[7px] text-[24px] text-[#A6A6A6]'>bobo@naver.com</div>
+                    </div>
+                    <div className="text-[16px]  mt-[17px] flex justify-between">
+                        <div className="rounded-full bg-[#FFE14F] b-1 px-4 py-1 text-[white]">
+                            {/* {tag} */}
+                            필라테스
+                        </div>
+                    </div>
                 </div>
 
-            </Modal>
-        </div>
-    );
-};
+                {/* 프로필 수정 모달 */}
+                <Modal
+                    isOpen={isModalOpen}
+                    onRequestClose={closeModal}
+                    style={modalStyles}
+                    contentLabel="Profile Modal"
+                >
+                    <div className='flex flex-col items-center'>
+                        <h2 className='mb-10 text-[20px] font-medium'>프로필 수정</h2>
+                        <div className='flex items-center mb-5 w-full'>
+                            <div className='w-[30%]'>
+                                <p className='font-medium'>닉네임</p></div>
+                            {/* <input placeholder={nickname} class="int w-[70%] h-10 rounded-lg px-3.5 py-2 shadow"></input> */}
+                        </div>
+                        <div className='flex items-center mb-5 w-full'>
+                            {/* 고정값 */}
+                            <div className='w-[30%]'>
+                                <p className='font-medium'>이메일</p></div>
+                            <div class="int h-10 rounded-lg px-3.5 py-2 w-[70%] shadow">bobo@naver.com</div>
+                        </div>
+                        <div className='flex items-center mb-[180px] w-full'>
+                            <div className='font-medium'>관심사</div>
+                            <div>
 
+                            </div>
+                        </div>
+                        <button onClick={closeModal} className='bg-[#FF7F1E] text-white rounded-[50px] w-20 h-10 mb-[10px] shadow hover:shadow-lg'>저장</button>
+                    </div>
+
+                </Modal>
+            </div>
+        );
+    
+}
 export default UserProfile;

8d23ce480dbd42a6b865cf6ff6512161aaf1a7a8
HaeJinS2 <9674qzq@gmail.com>
2023-06-10 03:21:35 +0900

    Design: Add oneday main image

--- client/src/pages/Oneday.jsx
+++ client/src/pages/Oneday.jsx
@@ -152,8 +152,15 @@ function Oneday() {
         </BodyContainer>
         <div className="flex justify-center items-center">
           <section className="absolute top-52 h-auto min-w-[1920px]">
-            <div className="bg-neutral-200 text-5xl font-sans font-semibold gap-4 flex flex-col justify-center items-center h-[600px]">
-              <p>당신의 특별한 하루</p>
+          <div
+              className="bg-neutral-200 text-5xl font-sans font-semibold gap-4 flex flex-col justify-center items-center pb-16 h-[600px] text-white"
+              style={{
+                backgroundImage: `url(${process.env.PUBLIC_URL}/images/oneday/oneday_main.png)`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+              }}
+            >              <p>당신의 특별한 하루</p>
               <p>'하루속'에서 함께하세요!</p>
             </div>
           </section>
